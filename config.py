08df69326e3c652b66040c247af2428d37078313
beejdrik <brntfader@gmail.com>
2026-02-23 01:24:02 -0500

    config work

--- config.py
+++ config.py
@@ -5,5 +5,6 @@ load_dotenv()
 
 class Config:
     SECRET_KEY = os.environ.get('SECRET_KEY')
+    STEAM_API_KEY = os.environ.get('STEAM_API_KEY')
     SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
     SQLALCHEMY_TRACK_MODIFICATIONS = False
